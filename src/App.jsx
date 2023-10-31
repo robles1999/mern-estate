c752659d08d0faf5b5cd737eaf6aa94b4dcfa220
Luis Robles <robles1999@yahoo.com>
2023-10-31 16:12:48 -0400

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// importing ea. page the router will be using
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/about';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-red-900'>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
