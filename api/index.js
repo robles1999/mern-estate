6b36bef9a1a22577195cac386e6e9c1910ec00ad
robles1999 <robles1999@yahoo.com>
2023-11-01 12:28:34 -0400

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -15,6 +15,17 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+// middleware to handle errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
